20b35b2c4e1cd5cd1302d0fe20548a1262e8d03a
Nishant Jha <me@nishantjha.org>
2020-01-29 21:36:16 -0500

    [WIP] minimizing

--- _updated/tests/test_page.py
+++ _updated/tests/test_page.py
@@ -3,5 +3,5 @@ from .helpers import TestCase
 
 class TestPage(TestCase):
     def test_header(self):
-        rv = self.client.get('/')
-        assert "Hello world!" in rv.data
+        rv = self.client.get("/")
+        assert "Hello world!" in str(rv.data)
