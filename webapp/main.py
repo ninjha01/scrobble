b8c0cf5946e41ebaa7db5224211a28bc7997d9fb
Nishant Jha <me@nishantjha.org>
2020-02-06 23:01:47 -0500

    [wip] gcp

--- webapp/main.py
+++ webapp/main.py
@@ -1,20 +1,20 @@
-from oauth2client.service_account import ServiceAccountCredentials
-from google.oauth2 import service_account  #
+# from oauth2client.service_account import ServiceAccountCredentials
+# from google.oauth2 import service_account  #
 
 import os
 import yaml
 
 from .app import create_app
 
-key_path = "secret.json"
-credentials = ServiceAccountCredentials.from_json_keyfile_name(key_path)
-credentials2 = service_account.Credentials.from_service_account_file(key_path)
+# key_path = "secret.json"
+# credentials = ServiceAccountCredentials.from_json_keyfile_name(key_path)
+# credentials2 = service_account.Credentials.from_service_account_file(key_path)
 
 config = {
-    "GCS_ROOT_PATH": os.environ["GCS_ROOT_PATH"],
-    "DATASTORE_PROJECT": "flask-template",
-    "GCP_CREDENTIALS": credentials,
-    "GCP_CREDENTIALS_2": credentials2,
+    #    "GCS_ROOT_PATH": os.environ["GCS_ROOT_PATH"],
+    #    "DATASTORE_PROJECT": "flask-template",
+    #    "GCP_CREDENTIALS": credentials,
+    #    "GCP_CREDENTIALS_2": credentials2,
     "SECRET_KEY": os.environ["SECRET_KEY"],
 }
 
