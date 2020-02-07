050a8eafdb5e14de6a18c567954abfce87b5b886
Nishant Jha <me@nishantjha.org>
2020-02-06 23:18:03 -0500

    [wip] gcp

--- webapp/main.py
+++ webapp/main.py
@@ -1,20 +1,16 @@
-# from oauth2client.service_account import ServiceAccountCredentials
-# from google.oauth2 import service_account  #
-
 import os
 import yaml
 
 from .app import create_app
 
-# key_path = "secret.json"
-# credentials = ServiceAccountCredentials.from_json_keyfile_name(key_path)
-# credentials2 = service_account.Credentials.from_service_account_file(key_path)
+try:
+    import googleclouddebugger
+
+    googleclouddebugger.enable()
+except ImportError:
+    pass
 
 config = {
-    #    "GCS_ROOT_PATH": os.environ["GCS_ROOT_PATH"],
-    #    "DATASTORE_PROJECT": "flask-template",
-    #    "GCP_CREDENTIALS": credentials,
-    #    "GCP_CREDENTIALS_2": credentials2,
     "SECRET_KEY": os.environ["SECRET_KEY"],
 }
 
