6eb4620a42ebbc48c544127fd5fa393744814079
Nishant Jha <ninjha01@gmail.com>
2020-02-07 21:25:40 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,4 @@ fi
 export FLASK_DEBUG=TRUE
 export FLASK_APP=main.py
 export GCS_ROOT_PATH='gs://flask-template/uploads'
-export SECRET_KEY="1hafs;dkljfads;ojsadffa78733314yashldfiff66zxvbj352sd0014--0423829yv.zxv.4GASD1234SDF231"
 flask run 
